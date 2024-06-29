eb36798bc515e14f996daae832749cd5751eed49
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-06-30 01:46:20 +0300

    all file

---
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -0,0 +1,25 @@
+import App from './App';
+import { shallow } from 'enzyme';
+import React from 'react';
+
+describe('App components', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders a div with the class App-header', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App-header')).toBeDefined();
+  });
+
+  it('renders a div with the class App-body', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App-body')).toBeDefined();
+  });
+
+  it('renders a div with the class App-footer', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App-footer')).toBeDefined();
+  });
+});
