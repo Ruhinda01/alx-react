a60ff46744db5b1143ad689cc850fc4bba73d5e0
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-07-06 02:47:34 +0300

    fix

---
+++ 0x03-React_component/task_1/dashboard/src/App/App.test.js
@@ -0,0 +1,64 @@
+import App from './App';
+import { shallow, mount } from 'enzyme';
+import React from 'react';
+import Notification from '../Notifications/Notifications';
+import Login from '../Login/Login';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
+
+
+const listCourses = [
+  {id: 1, name: 'ES6', credit: 60},
+  {id: 2, name: 'Webpack', credit: 20},
+  {id: 3, name: 'React', credit: 40}
+];
+
+describe('App components', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders Notifications', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.contains(<Notification />)).toBe(false);
+  });
+
+  it('renders Header', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.contains(<Header />)).toBe(true);
+  });
+
+  it('renders Login', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.contains(<Login />)).toBe(true);
+  });
+
+  it('renders Footer', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.contains(<Footer />)).toBe(true);
+  });
+
+  it('renders CourseList when isLoggedIn is true', () => {
+    const wrapper = shallow(<App isLoggedIn={true} />);
+    expect(wrapper.contains(<CourseList listCourses={listCourses} />)).toBe(true);
+  });
+
+  it('renders Login when isLoggedIn is false', () => {
+    const wrapper = shallow(<App isLoggedIn={false} />);
+    expect(wrapper.contains(<Login />)).toBe(true);
+  })
+
+});
+
+describe('App Props', () => {
+  it('verify that when the keys control and h are pressed, the function logOut is called', () => {
+    const mocked = jest.fn();
+    const wrapper = mount(<App logOut={mocked} />);
+    const event = new KeyboardEvent("keydown", { ctrlKey: true, key: "h" });
+    document.dispatchEvent(event);
+    expect(mocked).toHaveBeenCalledTimes(1);
+    wrapper.unmount();
+  });
+});
